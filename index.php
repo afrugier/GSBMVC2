f30b2ceeeb1a107d07059b07024dbbcfb3b284bf
afrugier <moi@pc>
2014-11-06 17:10:21 +0100

    premier essai commit

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-//premier commit
+//deuxieme commit
 require_once("include/fct.inc.php");
 require_once ("include/class.pdogsb.inc.php");
 include("vues/v_entete.php") ;
@@ -23,4 +23,3 @@ switch($uc){
 }
 include("vues/v_pied.php") ;
 ?>
-
